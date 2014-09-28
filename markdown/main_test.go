79fd9ccbf6905337e58330a4164e3d518d0c8597
Dmitri Shuralyov <shurcooL@gmail.com>
2014-09-27 17:14:39 -0700

    Add failing test case for nested lists.

--- markdown/main_test.go
+++ markdown/main_test.go
@@ -196,6 +196,48 @@ The outer pipes (|) are optional, and you don't need to make the raw Markdown li
 | *Still*  | ` + "`renders`" + ` | **nicely** |
 | 1        | 2         | 3          |
 
+Nested Lists
+============
+
+### Codeblock within list
+
+-	list1
+
+	` + "```" + `C
+	if (i == 5)
+	    break;
+	` + "```" + `
+
+### Blockquote within list
+
+-	list1
+
+	> This a quote within a list.
+
+### Table within list
+
+-	list1
+
+	| Header One | Header Two |
+	|------------|------------|
+	| Item One   | Item Two   |
+
+### Multi-level nested
+
+-	Item 1
+
+	Another paragraph inside this list item is indented just like the previous paragraph.
+-	Item 2
+	-	Item 2a
+
+		Things go here.
+
+		> This a quote within a list.
+
+		And they stay here.
+	-	Item 2b
+-	Item 3
+
 Line Breaks
 ===========
 
