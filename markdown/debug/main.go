4ba0068d3861f091f13004522b1ce5a9fcb6e21f
Dmitri Shuralyov <shurcooL@gmail.com>
2015-07-18 13:09:35 -0700

    Clean up debug package.

--- markdown/debug/main.go
+++ markdown/debug/main.go
@@ -1,4 +1,4 @@
-// This package provides a debug renderer. It prints the method names and parameters as they're called.
+// Package debug provides a debug renderer. It prints the method names and parameters as they're called.
 package debug
 
 import (
@@ -6,7 +6,7 @@ import (
 	"fmt"
 
 	"github.com/russross/blackfriday"
-	. "github.com/shurcooL/go/gists/gist6418290"
+	"github.com/shurcooL/go/gists/gist6418290"
 )
 
 type debugRenderer struct {
@@ -18,118 +18,118 @@ func NewRenderer(realRenderer blackfriday.Renderer) *debugRenderer {
 }
 
 func (dr *debugRenderer) BlockCode(out *bytes.Buffer, text []byte, lang string) {
-	fmt.Println(GetParentFuncArgsAsString(string(text), lang))
+	fmt.Println(gist6418290.GetParentFuncArgsAsString(string(text), lang))
 	dr.real.BlockCode(out, text, lang)
 }
 func (dr *debugRenderer) BlockQuote(out *bytes.Buffer, text []byte) {
-	fmt.Println(GetParentFuncArgsAsString(string(text)))
+	fmt.Println(gist6418290.GetParentFuncArgsAsString(string(text)))
 	dr.real.BlockQuote(out, text)
 }
 func (_ *debugRenderer) BlockHtml(out *bytes.Buffer, text []byte) {
-	fmt.Println(GetParentFuncArgsAsString(string(text)))
+	fmt.Println(gist6418290.GetParentFuncArgsAsString(string(text)))
 }
 func (_ *debugRenderer) TitleBlock(out *bytes.Buffer, text []byte) {
-	fmt.Println(GetParentFuncArgsAsString(string(text)))
+	fmt.Println(gist6418290.GetParentFuncArgsAsString(string(text)))
 }
 func (_ *debugRenderer) Header(out *bytes.Buffer, text func() bool, level int, id string) {
-	fmt.Println(GetParentFuncArgsAsString(level, id))
+	fmt.Println(gist6418290.GetParentFuncArgsAsString(level, id))
 }
 func (_ *debugRenderer) HRule(out *bytes.Buffer) {
-	fmt.Println(GetParentFuncArgsAsString())
+	fmt.Println(gist6418290.GetParentFuncArgsAsString())
 }
 func (dr *debugRenderer) List(out *bytes.Buffer, text func() bool, flags int) {
-	fmt.Println(GetParentFuncArgsAsString(flags))
+	fmt.Println(gist6418290.GetParentFuncArgsAsString(flags))
 	debugText := func() bool {
 		b := text()
-		fmt.Println("text", GetParentFuncArgsAsString(), "->", b)
+		fmt.Println("text", gist6418290.GetParentFuncArgsAsString(), "->", b)
 		return b
 	}
 	dr.real.List(out, debugText, flags)
 }
 func (dr *debugRenderer) ListItem(out *bytes.Buffer, text []byte, flags int) {
-	fmt.Println(GetParentFuncArgsAsString(string(text), flags))
+	fmt.Println(gist6418290.GetParentFuncArgsAsString(string(text), flags))
 	dr.real.ListItem(out, text, flags)
 }
 func (dr *debugRenderer) Paragraph(out *bytes.Buffer, text func() bool) {
-	fmt.Println(GetParentFuncArgsAsString())
+	fmt.Println(gist6418290.GetParentFuncArgsAsString())
 	debugText := func() bool {
 		b := text()
-		fmt.Println("text", GetParentFuncArgsAsString(), "->", b)
+		fmt.Println("text", gist6418290.GetParentFuncArgsAsString(), "->", b)
 		return b
 	}
 	dr.real.Paragraph(out, debugText)
 }
 func (_ *debugRenderer) Table(out *bytes.Buffer, header []byte, body []byte, columnData []int) {
-	fmt.Println(GetParentFuncArgsAsString(string(header), string(body), columnData))
+	fmt.Println(gist6418290.GetParentFuncArgsAsString(string(header), string(body), columnData))
 }
 func (_ *debugRenderer) TableRow(out *bytes.Buffer, text []byte) {
-	fmt.Println(GetParentFuncArgsAsString(string(text)))
+	fmt.Println(gist6418290.GetParentFuncArgsAsString(string(text)))
 }
 func (_ *debugRenderer) TableHeaderCell(out *bytes.Buffer, text []byte, align int) {
-	fmt.Println(GetParentFuncArgsAsString(string(text), align))
+	fmt.Println(gist6418290.GetParentFuncArgsAsString(string(text), align))
 }
 func (_ *debugRenderer) TableCell(out *bytes.Buffer, text []byte, align int) {
-	fmt.Println(GetParentFuncArgsAsString(string(text), align))
+	fmt.Println(gist6418290.GetParentFuncArgsAsString(string(text), align))
 }
 func (_ *debugRenderer) Footnotes(out *bytes.Buffer, text func() bool) {
-	fmt.Println(GetParentFuncArgsAsString())
+	fmt.Println(gist6418290.GetParentFuncArgsAsString())
 }
 func (_ *debugRenderer) FootnoteItem(out *bytes.Buffer, name, text []byte, flags int) {
-	fmt.Println(GetParentFuncArgsAsString(string(name), string(text), flags))
+	fmt.Println(gist6418290.GetParentFuncArgsAsString(string(name), string(text), flags))
 }
 
 func (_ *debugRenderer) AutoLink(out *bytes.Buffer, link []byte, kind int) {
-	fmt.Println(GetParentFuncArgsAsString(string(link), kind))
+	fmt.Println(gist6418290.GetParentFuncArgsAsString(string(link), kind))
 }
 func (_ *debugRenderer) CodeSpan(out *bytes.Buffer, text []byte) {
-	fmt.Println(GetParentFuncArgsAsString(string(text)))
+	fmt.Println(gist6418290.GetParentFuncArgsAsString(string(text)))
 }
 func (_ *debugRenderer) DoubleEmphasis(out *bytes.Buffer, text []byte) {
-	fmt.Println(GetParentFuncArgsAsString(string(text)))
+	fmt.Println(gist6418290.GetParentFuncArgsAsString(string(text)))
 }
 func (_ *debugRenderer) Emphasis(out *bytes.Buffer, text []byte) {
-	fmt.Println(GetParentFuncArgsAsString(string(text)))
+	fmt.Println(gist6418290.GetParentFuncArgsAsString(string(text)))
 }
 func (_ *debugRenderer) Image(out *bytes.Buffer, link []byte, title []byte, alt []byte) {
-	fmt.Println(GetParentFuncArgsAsString(string(link), string(title), string(alt)))
+	fmt.Println(gist6418290.GetParentFuncArgsAsString(string(link), string(title), string(alt)))
 }
 func (dr *debugRenderer) LineBreak(out *bytes.Buffer) {
-	fmt.Println(GetParentFuncArgsAsString())
+	fmt.Println(gist6418290.GetParentFuncArgsAsString())
 	dr.real.LineBreak(out)
 }
 func (_ *debugRenderer) Link(out *bytes.Buffer, link []byte, title []byte, content []byte) {
-	fmt.Println(GetParentFuncArgsAsString(string(link), string(title), string(content)))
+	fmt.Println(gist6418290.GetParentFuncArgsAsString(string(link), string(title), string(content)))
 }
 func (_ *debugRenderer) RawHtmlTag(out *bytes.Buffer, tag []byte) {
-	fmt.Println(GetParentFuncArgsAsString(string(tag)))
+	fmt.Println(gist6418290.GetParentFuncArgsAsString(string(tag)))
 }
 func (_ *debugRenderer) TripleEmphasis(out *bytes.Buffer, text []byte) {
-	fmt.Println(GetParentFuncArgsAsString(string(text)))
+	fmt.Println(gist6418290.GetParentFuncArgsAsString(string(text)))
 }
 func (_ *debugRenderer) StrikeThrough(out *bytes.Buffer, text []byte) {
-	fmt.Println(GetParentFuncArgsAsString(string(text)))
+	fmt.Println(gist6418290.GetParentFuncArgsAsString(string(text)))
 }
 func (_ *debugRenderer) FootnoteRef(out *bytes.Buffer, ref []byte, id int) {
-	fmt.Println(GetParentFuncArgsAsString(string(ref), id))
+	fmt.Println(gist6418290.GetParentFuncArgsAsString(string(ref), id))
 }
 
 func (dr *debugRenderer) Entity(out *bytes.Buffer, entity []byte) {
-	fmt.Println(GetParentFuncArgsAsString(string(entity)))
+	fmt.Println(gist6418290.GetParentFuncArgsAsString(string(entity)))
 	dr.real.Entity(out, entity)
 }
 func (dr *debugRenderer) NormalText(out *bytes.Buffer, text []byte) {
-	fmt.Println(GetParentFuncArgsAsString(string(text)))
+	fmt.Println(gist6418290.GetParentFuncArgsAsString(string(text)))
 	dr.real.NormalText(out, text)
 }
 
 func (_ *debugRenderer) DocumentHeader(out *bytes.Buffer) {
-	fmt.Println(GetParentFuncArgsAsString())
+	fmt.Println(gist6418290.GetParentFuncArgsAsString())
 }
 func (_ *debugRenderer) DocumentFooter(out *bytes.Buffer) {
-	fmt.Println(GetParentFuncArgsAsString())
+	fmt.Println(gist6418290.GetParentFuncArgsAsString())
 }
 
 func (dr *debugRenderer) GetFlags() int {
-	fmt.Println(GetParentFuncArgsAsString())
+	fmt.Println(gist6418290.GetParentFuncArgsAsString())
 	return dr.real.GetFlags()
 }
