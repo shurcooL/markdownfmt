f0dd669bd77bd7ba90aa3892dd4c00f55e15a131
Tianon Gravi <admwiggin@gmail.com>
2016-01-15 15:45:56 -0800

    Simple little hack to fix code block in a list at the end of a file causing a spurious blank line

--- main.go
+++ main.go
@@ -66,6 +66,7 @@ func processFile(filename string, in io.Reader, out io.Writer, stdin bool) error
 	if err != nil {
 		return err
 	}
+	res = []byte(strings.TrimSpace(string(res)) + "\n")
 
 	if !bytes.Equal(src, res) {
 		// formatting has changed
