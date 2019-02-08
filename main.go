659d8eb99ceae372604426ec587f25319e2210aa
Tianon Gravi <admwiggin@gmail.com>
2019-02-08 08:10:52 -0800

    If a file is empty, leave it empty (zero bytes)

--- main.go
+++ main.go
@@ -66,7 +66,9 @@ func processFile(filename string, in io.Reader, out io.Writer, stdin bool) error
 	if err != nil {
 		return err
 	}
-	res = []byte(strings.TrimSpace(string(res)) + "\n")
+	if len(res) > 0 {
+		res = []byte(strings.TrimSpace(string(res)) + "\n")
+	}
 
 	if !bytes.Equal(src, res) {
 		// formatting has changed
